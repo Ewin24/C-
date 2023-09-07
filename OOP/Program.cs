42e2364403223128151567472e80767ac1acbcb4
ewin24 <edwintrigos24@gmail.com>
2023-09-07 13:12:59 -0500

    TODO: empezar la funcionalidad de reportes

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -7,72 +7,39 @@ internal class Program
     {
         List<Estudiante> estudiantes = new List<Estudiante>();
         Funcionalidad funciones = new Funcionalidad();
-        int opc = 4;
-        do
+        try
         {
-            //MENU
-            opc = funciones.menu();
-
-            switch (opc)
+            int opc = 4;
+            do
             {
-                case 1:
-                    Console.WriteLine("\n");
-                    String? codigo = Guid.NewGuid().ToString();
-                    Console.Write("Ingrese el nombre del estudiante:");
-                    String? nombre = Console.ReadLine();
-                    if (nombre.Length > 40 || nombre.Length < 0)
-                    {
-                        Console.WriteLine("El nombre no puede superar 40 caracteres");
-                        break;
-                    }
-                    Console.Write("Ingrese el correo del estudiante:");
-                    String? correo = Console.ReadLine();
-                    if (correo.Length > 40)
-                    {
-                        Console.WriteLine("El correo no puede superar 40 caracteres");
-                        break;
+                //MENU
+                opc = funciones.menu();
 
-                    }
-                    Console.Write("Ingrese la edad del estudiante:");
-                    int edad = Int32.Parse(Console.ReadLine());
-                    if (edad < 0)
-                    {
-                        Console.WriteLine("El estudiante no puede tener menos de 1 año");
+                switch (opc)
+                {
+                    case 1:
+                        funciones.agregarEstudiantes(estudiantes);
                         break;
-                    }
-                    Console.Write("Ingrese la direccion del estudiante:");
-                    String direccion = Console.ReadLine();
-                    if (direccion.Length > 35)
-                    {
-                        Console.WriteLine("La direccion no puede superar 35 caracteres");
+                    case 2:
+                        funciones.listarEstudiantes(estudiantes);
                         break;
-                    }
-                    Estudiante estudiante = new Estudiante(codigo, nombre, edad, correo, direccion);
-                    estudiantes.Add(estudiante);
-                    break;
-                case 2:
-                    for (int i = 0; i < estudiantes.Count; i++)
-                    {
-                        Console.WriteLine(estudiantes[i].codigo);
-                        Console.WriteLine(estudiantes[i].nombre);
-                        Console.WriteLine(estudiantes[i].edad);
-                        Console.WriteLine(estudiantes[i].correo);
-                        Console.WriteLine(estudiantes[i].direccion);
-                        // Console.WriteLine(" {0,10}{0,10}", "Cod Estudiante", "Nombre del estudiante");
-                        // Console.WriteLine(" {" + "0," + "0" + "} {" + 0 + ",longi" + "}", "Menor1", "papa1", "papa1");
-                    }
-                    break;
-            }
-        } while (opc != 4);
-
+                }
+            } while (opc != 4);
+        }
+        catch (System.Exception e)
+        {
+            Console.WriteLine(e.ToString());
+            funciones.menu();
+            throw;
+        }
     }
     //CRUD
     // un profesor necesita registrar estudiantes matriculados, la informacion que el docente posee de cada estudiante es:
-    // TODO: codigo del estudiante con longitud max de 15 caracteres
-    // TODO: nombre del estudiante con longitud max de 40 caracteres
-    // TODO: correo del estudiante con longitud max de 40 caracteres
-    // TODO: edad del estudiante 
-    // TODO: direccion del estudiante con longitud max de 35 caracteres
+    //  codigo del estudiante con longitud max de 15 caracteres
+    //  nombre del estudiante con longitud max de 40 caracteres
+    //  correo del estudiante con longitud max de 40 caracteres
+    //  edad del estudiante 
+    //  direccion del estudiante con longitud max de 35 caracteres
     //REPORTES
     //TODO: se tiene como norma que cada estudiante presenta 4 quices 2 trabajos y 3 parciales
     //TODO: las notas de los quices equivalen al 15%
@@ -81,6 +48,9 @@ internal class Program
     //TODO: se le permite al profesor generar los reportes:
     //TODO: listado general de notas paginado por 10
     //TODO: el programa debe permitir registro por nota, quiz, parciales
+
+    //FUNCIONALIDADES
+    //TODO: Hacer que el programa no se detenga en caso de que se ingresen valores no correctos
 }
 
 
--- OOP/logic/Funcionalidad.cs
+++ OOP/logic/Funcionalidad.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using OOP.Entities;
 
 namespace OOP.logic
 {
@@ -22,6 +23,62 @@ namespace OOP.logic
             opc = Int32.Parse(Console.ReadLine());
             Console.WriteLine("\n");
             return opc;
+        }
+
+        public void agregarEstudiantes(List<Estudiante> estudiantes)
+        {
+            Console.WriteLine("\n");
+
+            String? codigo = Console.ReadLine();
+            if (codigo.Length > 15 || codigo.Length < 0)
+            {
+                Console.WriteLine("El nombre no puede superar 15 caracteres o ser menor a 1 caracter");
+                return;
+            }
+            Console.Write("Ingrese el nombre del estudiante:");
+            String? nombre = Console.ReadLine();
+            if (nombre.Length > 40 || nombre.Length < 0)
+            {
+                Console.WriteLine("El nombre no puede superar 40 caracteres o ser menor a 1 caracter");
+                return;
+            }
+            Console.Write("Ingrese el correo del estudiante:");
+            String? correo = Console.ReadLine();
+            if (correo.Length > 40 || correo.Length < 0)
+            {
+                Console.WriteLine("El correo no puede superar 40 caracteres o ser menor a 1 caracter");
+                return;
+            }
+            Console.Write("Ingrese la edad del estudiante:");
+            int edad = Int32.Parse(Console.ReadLine());
+            if (edad < 0)
+            {
+                Console.WriteLine("El estudiante no puede tener menos de 1 año");
+                return;
+            }
+            Console.Write("Ingrese la direccion del estudiante:");
+            String direccion = Console.ReadLine();
+            if (direccion.Length > 35 || direccion.Length < 0)
+            {
+                Console.WriteLine("La direccion no puede superar 35 caracteres o ser menor a 1 caracter");
+                return;
+            }
+            Estudiante estudiante = new Estudiante(codigo, nombre, edad, correo, direccion);
+            estudiantes.Add(estudiante);
+        }
+
+        public void listarEstudiantes(List<Estudiante> estudiantes)
+        {
+            for (int i = 0; i < estudiantes.Count; i++)
+            {
+                Console.WriteLine(estudiantes[i].codigo);
+                Console.WriteLine(estudiantes[i].nombre);
+                Console.WriteLine(estudiantes[i].edad);
+                Console.WriteLine(estudiantes[i].correo);
+                Console.WriteLine(estudiantes[i].direccion);
+                // Console.WriteLine(" {0,10}{0,10}", "Cod Estudiante", "Nombre del estudiante");
+                // Console.WriteLine(" {" + "0," + "0" + "} {" + 0 + ",longi" + "}", "Menor1", "papa1", "papa1");
+            }
         }
     }
 }
