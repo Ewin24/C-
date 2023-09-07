eafb7ccf27123beec91f6e6c23be18e99bd23937
ewin24 <edwintrigos24@gmail.com>
2023-09-07 07:06:47 -0500

    .

---
+++ OOP/Entities/Estudiante.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOP.Entities
+{
+    public class Estudiante
+    {
+        private string id;
+        private string nombre;
+        private string apellido;
+        private int grado;
+    }
+    
+}
