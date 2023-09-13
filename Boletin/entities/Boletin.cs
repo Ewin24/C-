c253a9a5b8243a679551db0ee702d7b22aae7bd2
ewin24 <edwintrigos24@gmail.com>
2023-09-13 06:45:54 -0500

    feat: update boletin

--- Boletin/entities/Boletin.cs
+++ Boletin/entities/Boletin.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Boletin.entities
+namespace Boletin.Entities
 {
     public class Boletin
     {
-        private 
+        List<float> quices = new List<float>();
+        List<float> trabajos = new List<float>();
+        List<float> parciales = new List<float>();
+
+        public Boletin()
+        {
+        }
+
+        public Boletin(List<float> quices, List<float> trabajos, List<float> parciales)
+        {
+            this.Quices = quices;
+            this.Trabajos = trabajos;
+            this.Parciales = parciales;
+        }
+
+        public List<float> Quices { get => quices; set => quices = value; }
+        public List<float> Trabajos { get => trabajos; set => trabajos = value; }
+        public List<float> Parciales { get => parciales; set => parciales = value; }
     }
+
 }
